de00e28e2ec50fa29f2e8027d9bbe35bcdc31beb
jjlg90 <jjlimag2@gmail.com>
2021-10-25 17:35:04 +0000

    Added wishlist view, models aand emplate

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from django.contrib import messages
+
+from products.models import Product
+from .models import WishList
+
+
+@login_required
+def wishlist(request):
+    """
+    A view to render the user's wishlist
+    """
+    wishlist = None
+    try:
+        wishlist = WishList.objects.get(user=request.user)
+    except WishList.DoesNotExist:
+        pass
+
+    context = {
+        'wishlist': wishlist,
+    }
+
+    return render(request, 'wishlist/wishlist.html', context)
