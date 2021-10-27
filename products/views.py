56742ecab24b374989fef2253930c6e33bc69ccc
jjlg90 <jjlimag2@gmail.com>
2021-10-27 11:45:26 +0000

    Added edit review functionality

--- products/views.py
+++ products/views.py
@@ -101,6 +101,39 @@ def add_review(request, product_id):
 
 
 @login_required
+def edit_review(request, review_id):
+    """
+    A view to allow the users to edit their own review
+    """
+
+    review = get_object_or_404(ProductReview, pk=review_id)
+    product = review.product
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Review has been changed')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request, 'Review edit failed, Please try again')
+
+    else:
+        form = ReviewForm(instance=review)
+
+    messages.info(request, 'You are editing your review')
+    template = 'products/product_detail.html'
+    context = {
+        'form': form,
+        'review': review,
+        'product': product,
+        'edit': True,
+    }
+    return render(request, template, context)
+
+
+@login_required
 def add_product(request):
     """ Add a product to the store """
 
