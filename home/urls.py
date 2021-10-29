2fc1516139c61ae4b907a379ee50e544a5cb0b8e
jjlg90 <jjlimag2@gmail.com>
2021-10-29 15:09:16 +0000

    Added contact form functionality

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import contact, success
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path("contact", views.contact, name="contact"),
+    path('success/', views.success, name='success'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -7,3 +10,36 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Inquiry"
+            body = {
+                'first_name': form.cleaned_data['first_name'],
+                'last_name': form.cleaned_data['last_name'],
+                'email': form.cleaned_data['email_address'],
+                'message': form.cleaned_data['message'],
+            }
+            message = "\n".join(body.values())
+
+            try:
+                send_mail(
+                    subject, message, 'admin@example.com', [
+                        'admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+
+            return redirect("success")
+
+    form = ContactForm()
+    return render(request, "home/contact.html", {'form': form})
+
+
+def success(request):
+    """
+    A view to return the success page after an email was sent.
+    """
+    return render(request, 'home/contact_success.html')
