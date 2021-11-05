3d2b4f6e2c2c698a743066f61efbb63b006cec50
jjlg90 <jjlimag2@gmail.com>
2021-11-05 11:57:42 +0000

    Removed secret key and set debug

--- flavors_from_home/settings.py
+++ flavors_from_home/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+1=fydedbf+rxp*n^^%3k2up4bcb=ioerhazgcky-p2(j(5ef^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['flavors-from-home.herokuapp.com', 'localhost']
 
